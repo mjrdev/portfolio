df47ca3bfb9202520cc85bc72c51aeae2d740462
mjrdev <jrgomesdev@gmail.com>
2022-09-04 02:19:22 -0300

    mod docs

---
+++ docs/_nuxt/static/1662268708/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1662268708",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
