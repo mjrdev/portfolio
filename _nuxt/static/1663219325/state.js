78c00c9fea90714d61129821ae1b89d8cfa780ad
mjrdev <mjrdev@users.noreply.github.com>
2022-09-15 05:22:11 +0000

    deploy: 50fc1f977cb9bcd355447bb69071081e49c39210

---
+++ _nuxt/static/1663219325/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1663219325",layout:"default",error:a,state:{user:{name:"Manoel Júnior",username:"mjrdev"}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
