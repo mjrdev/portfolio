c26fcf81ac9263049692e0889cc5a9485f9713cb
mjrdev <jrgomesdev@gmail.com>
2022-09-24 17:47:39 -0300

    improvent projects, add slider projects mobile

--- components/abas/projects/Projects.vue
+++ components/abas/projects/Projects.vue
@@ -1,30 +1,73 @@
 <script>
-    import Item from './Item'
     import projectsList from './projectsList.js'
     
     export default {
         name: "Projects",
-        components: { Item },
         data() {
             return {
                 projectsList,
-                carouselItemsActive: [
-                    true, true, true, true
-                ]
+                indexItemActive: 0,
+                carouselScrollActive: true,
+                animation: false,
             }
         },
+        methods: {
+            carouselScroll(direction) {
+
+                switch (direction) {
+                    case 'right':
+                        {
+                            if(this.indexItemActive < projectsList.length - 1 && this.carouselScrollActive) {
+                                this.indexItemActive++
+                                this.animation = true
+                                this.carouselScrollActive = false
+                            }
+                        }
+                        break;
+                    case 'left':
+                        {
+                            if(this.indexItemActive > 0 && this.carouselScrollActive) {
+                                this.indexItemActive--
+                                this.animation = true
+                                this.carouselScrollActive = false
+                            }
+                        }
+                        break;
+                }
+
+
+                setTimeout(() => {
+                    this.animation = false
+                    this.carouselScrollActive = true
+                }, 400)
+            }
+        }
 }
 </script>
 
 <template>
-    <div class="projects">
-        <h1 class="title-projects" data-aos="fade-up">Projetos</h1>
+    <div class="projects" data-aos="fade-up">
+        <h1 class="title-projects" >Projetos</h1>
         
-        <div class="container-projects" data-aos="fade-up">
-            <div class="item-project"> project 1 </div>
-            <div class="item-project"> project 2 </div>
-            <div class="item-project"> project 3 </div>
-            <div class="item-project"> project 4 </div>
+
+        <div class="carousel">
+            <span class="material-symbols-outlined arrow" @click="carouselScroll('left')">chevron_left</span>
+                <div :class="{
+                    'item-project': true, 'animation-opacity': animation
+                }">
+                    <h1>{{projectsList[indexItemActive].title}}</h1>
+                    <p>{{projectsList[indexItemActive].description}}</p>
+                </div>
+            <span class="material-symbols-outlined arrow" @click="carouselScroll('right')">chevron_right</span>
+        </div>
+
+        <div class="container-projects">
+
+            <div class="item-project" v-for="(pjt, index) in projectsList">
+                <h1>{{pjt.title}}</h1>
+                <p>{{pjt.description}}</p>
+            </div>
+
         </div>
     </div>
 </template>
