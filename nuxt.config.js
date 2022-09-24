c2cbb800565595f40bfba30d96ed8a243ecd6813
mjrdev <jrgomesdev@gmail.com>
2022-09-24 18:56:12 -0300

    modify fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,10 @@ export default {
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0' },
-      { rel: 'stylesheet',
-      href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,300;1,500&family=Raleway:wght@500;700;800;900&display=swap" }
+      {
+        rel: 'stylesheet',
+        href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,300;1,500&family=Raleway:wght@500;700;800;900&family=Work+Sans:wght@300;400;600&display=swap"
+      }
     ]
   },
 
