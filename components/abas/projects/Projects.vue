c10f679c6ccae3b365802da2e77ff7ba6861ece8
mjrdev <jrgomesdev@gmail.com>
2022-09-23 23:34:19 -0300

    modify project

--- components/abas/projects/Projects.vue
+++ components/abas/projects/Projects.vue
@@ -7,82 +7,25 @@
         components: { Item },
         data() {
             return {
-                contract: true,
                 projectsList,
-                carouselCount: 0,
-                animation: false,
-                sizesDevices: {
-                    lg: 1000, md: 800, sm: 600
-                }
+                carouselItemsActive: [
+                    true, true, true, true
+                ]
             }
         },
-        computed: {
-        
-        },
-        methods: {
-            carouselUse: function(count) {
-                if(
-                    count == 1 && this.carouselCount + 3 < this.projectsList.length && this.animation == false
-                ) {
-                    this.carouselCount++
-                    this.carouselAnimation()
-                } else if (
-                    count == -1 && this.carouselCount > 0 && this.animation == false
-                ) {
-                    this.carouselCount--
-                    this.carouselAnimation()
-                }
-            },
-
-            carouselAnimation: function () {
-                const data = this
-                const time = setTimeout(function() {
-                    data.animation = false
-                }, 300)
-
-                this.animation = true
-            }
-    }
 }
 </script>
 
 <template>
-    <div class="container-skill" >
-        <h1 class="skill-title" data-aos="fade-up">Projetos</h1>
+    <div class="projects">
+        <h1 class="title-projects" data-aos="fade-up">Projetos</h1>
         
-        <div class="project-container" data-aos="fade-up">
-
-            <span class="material-symbols-outlined arrow"
-            @click="carouselUse(-1)"
-            :style="{
-                color: carouselCount == 0 ? '#b0b0b0' : '#424ed0'
-            }"
-            >
-                chevron_left
-            </span>
-
-            <div class="project-items">
-
-                <Item :class="{
-                    'project-item': true,
-                    'animation': animation
-                }"
-                v-for="(i, index) in projectsList"
-                v-if="index < size()"
-                :project="projectsList[index + carouselCount]"
-                />
-
-            </div>
-            
-            <span class="material-symbols-outlined arrow"
-            @click="carouselUse(1)"
-            :style="{
-                color: carouselCount + 3 == projectsList.length  ? '#b0b0b0' : '#424ed0'
-            }"
-            >chevron_right</span>
+        <div class="container-projects" data-aos="fade-up">
+            <div class="item-project"> project 1 </div>
+            <div class="item-project"> project 2 </div>
+            <div class="item-project"> project 3 </div>
+            <div class="item-project"> project 4 </div>
         </div>
-
-        
     </div>
 </template>
 
