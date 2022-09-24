b2bc5faf1fb3f55590082f21f670f6a9c8587dc1
mjrdev <mjrdev@users.noreply.github.com>
2022-09-24 21:57:35 +0000

    deploy: c2cbb800565595f40bfba30d96ed8a243ecd6813

--- _nuxt/0a0da6b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{296:function(n,e,o){"use strict";o.r(e),e.default={name:"Menu",props:{abas:Array},data:function(){return{menu:!1}},methods:{changeMenu:function(){console.log(this.menu),this.menu=!this.menu}}}}}]);
