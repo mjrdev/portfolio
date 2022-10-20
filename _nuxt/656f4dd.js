85a38758eeb4e6fdc8a637776165cdc66d9b2d2f
mjrdev <mjrdev@users.noreply.github.com>
2022-10-20 16:21:29 +0000

    deploy: 4f457d6067da3d48bb4f5c2e34cd83456fc1958c

---
+++ _nuxt/656f4dd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{294:function(n,e,o){"use strict";o.r(e),e.default={name:"Menu",props:{abas:Array},data:function(){return{menu:!1}},methods:{changeMenu:function(){console.log(this.menu),this.menu=!this.menu}}}}}]);
