3549ec867dc26176274a080097b2f1d5e5ed0108
mjrdev <jrgomesdev@gmail.com>
2022-10-25 12:38:40 -0300

    update

--- components/abas/projects/projectsList.js
+++ components/abas/projects/projectsList.js
@@ -2,7 +2,7 @@ export default [
     { title: 'Meu Portfolio', urlImage: 'projects/portfolio.jpg', url: "https://mjrdev.github.io/portfolio" },
     { title: 'App de Cotações', urlImage: "projects/quotes.jpg", url: "https://mjrdev.github.io/quotes" },
     { title: 'API RESTFULL + Autenticação JWT', urlImage: 'projects/apirf-ts.jpg', url: "https://github.com/mjrdev/webapp-login" },
-    { title: 'Recriação Client Riot', urlImage: "projects/riot.png", url: "https://github.com/mjrdev/riotclient" },
+    { title: 'Recriação Client Riot', urlImage: "projects/riot.png", url: "https://mjrdev.github.io/riotclient/" },
     { title: 'App de Tarefas Diárias', urlImage: 'projects/tasks.jpg', url: "https://mjrdev.github.io/apptasks" },
     { title: 'Jogo PushBall', urlImage: "projects/em-breve.jpg", url: "https://mjrdev.github.io/portfolio" },
     { title: 'Tela de login', urlImage: 'projects/login.png', url: "https://mjrdev.github.io/login-screen" },
